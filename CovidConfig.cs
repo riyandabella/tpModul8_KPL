e5cf2795f2c4a9d0c39ded3ea694b782e8dd6187
riyandabella <riyandabella561@gmail.com>
2025-04-14 01:13:03 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_deman { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    private const string configPath = "covid_config.json";
+
+    
+    public static CovidConfig Load()
+    {
+        if (File.Exists(configPath))
+        {
+            var json = File.ReadAllText(configPath);
+            return JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+        else
+        {
+            return new CovidConfig
+            {
+                satuan_suhu = "celcius",
+                batas_hari_deman = 14,
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+            };
+        }
+
+}
+
+public void UbahSatuan()
+    {
+        satuan_suhu = satuan_suhu == "celcius" ? "fahrenheit" : "celcius";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("commit pertama");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = CovidConfig.Load();
+
+        config.UbahSatuan();
+
+        Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_suhu);
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            if (suhu >= 36.5 && suhu <= 37.5)
+                suhuNormal = true;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            if (suhu >= 97.7 && suhu <= 99.5)
+                suhuNormal = true;
+        }
+
+        bool hariAman = hariDemam < config.batas_hari_deman;
+
+        if (suhuNormal && hariAman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
